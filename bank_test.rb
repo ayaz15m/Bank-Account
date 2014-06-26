30328ebef02e54c54c4e537965f6e4c85ad7d19c
Ayaz Merchant <ayaz15m@yahoo.com>
2014-06-26 14:46:19 -0500

    Test-5-Green

--- bank_test.rb
+++ bank_test.rb
@@ -25,7 +25,9 @@ class BankTest < Minitest::Test
 
   def test_calculate_balance_in_account_after_withdrawal_and_deposit
     @bank.account(100)
-    withrawal_amount = @bank.withdraw(50)    
+    withrawal_amount = @bank.withdraw(50)
+    @bank.calculate_balance(withrawal_amount)
+        
     deposit_amount = @bank.deposit(75)
     assert_equal 125, @bank.calculate_balance(deposit_amount)
   end
