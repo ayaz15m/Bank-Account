b36e136c61f4127a35061888f78ebd2f8af9c2bf
Ayaz Merchant <ayaz15m@yahoo.com>
2014-06-26 14:40:25 -0500

    Test-4-Refactor

--- bank_test.rb
+++ bank_test.rb
@@ -13,17 +13,14 @@ class BankTest < Minitest::Test
 
   def test_calculate_balance_in_account_after_deposit
     @bank.account(100)
-    assert_equal 150, @bank.calculate_balance(@bank.deposit(50))
-  end
-
-  def test_amount_withdrawn_received_in_method
-    assert_equal -100, @bank.withdraw(100)
+    deposit_amount = @bank.deposit(50)
+    assert_equal 150, @bank.calculate_balance(deposit_amount)
   end
 
   def test_calculate_balance_in_account_after_withdrawal
     @bank.account(100)
-    amount = @bank.withdraw(50)
-    assert_equal 50, @bank.calculate_balance(amount)
+    withrawal_amount = @bank.withdraw(50)
+    assert_equal 50, @bank.calculate_balance(withrawal_amount)
   end
 
 end
