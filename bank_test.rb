97b7e31c5dfba2cca88d14ba9b456792cfce1994
Ayaz Merchant <ayaz15m@yahoo.com>
2014-06-26 14:37:33 -0500

    Test-4-Red

--- bank_test.rb
+++ bank_test.rb
@@ -11,13 +11,19 @@ class BankTest < Minitest::Test
     assert_equal 100, @bank.account(100)
   end
 
-  def test_balance_in_account_after_deposit
+  def test_calculate_balance_in_account_after_deposit
     @bank.account(100)
     assert_equal 150, @bank.calculate_balance(@bank.deposit(50))
   end
 
   def test_amount_withdrawn_received_in_method
     assert_equal 100, @bank.withdraw(100)
+  end
+
+  def test_calculate_balance_in_account_after_withdrawal
+    @bank.account(100)
+    amount = @bank.deposit(50)
+    assert_equal 50, @bank.calculate_balance(amount)
   end
 
 end
